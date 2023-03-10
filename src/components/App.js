9af9e1d2f31c53ba7921f6fb3934d87d1bac0f2d
YassinAbdulrahman <107113173+YassinAbdulrahman@users.noreply.github.com>
2023-03-10 18:01:28 +0300

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import Nav from "./Nav";
 
 import hogs from "../porkers_data";
+import HongList from "./HongList";
 
 function App() {
 	return (
 		<div className="App">
 			<Nav />
+			<HongList hogs={hogs}/>
 		</div>
 	);
 }
